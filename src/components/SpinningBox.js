5f7a85453b1078f8bc90fad3f7543cbdc2ed44a1
Shubidumdu <skaty4870@naver.com>
2021-06-04 13:56:39 +0900

    Add light on SpinningXox

--- src/components/SpinningBox.js
+++ src/components/SpinningBox.js
@@ -1,41 +1,47 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef } from 'react';
 import * as THREE from 'three';
 
 function SpinningBox() {
   const ref = useRef(null);
   useEffect(() => {
     const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera( 75, 1, 0.1, 1000 );
+    const camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000);
 
     const renderer = new THREE.WebGLRenderer();
-    renderer.setSize( 500, 500 );
-    ref && ref.current.append( renderer.domElement );
+    renderer.setSize(500, 500);
+    ref && ref.current.append(renderer.domElement);
 
+    const light = new THREE.DirectionalLight(0xffffff, 1);
+    light.position.set(-1, 2, 4);
+    scene.add(light);
     const geometry = new THREE.BoxGeometry();
-    const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    const cube = new THREE.Mesh( geometry, material );
-    scene.add( cube );
-  
+    const material = new THREE.MeshPhongMaterial({ color: 0x00ff00 });
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
     camera.position.z = 5;
 
     const animate = function () {
-      requestAnimationFrame( animate );
+      requestAnimationFrame(animate);
 
       cube.rotation.x += 0.01;
       cube.rotation.y += 0.01;
 
-      renderer.render( scene, camera );
+      renderer.render(scene, camera);
     };
 
     animate();
   }, [ref]);
 
   return (
-    <div ref={ref} style={{
-      width: 500,
-      height: 500
-    }}/>
-  )
+    <div
+      ref={ref}
+      style={{
+        width: 500,
+        height: 500,
+      }}
+    />
+  );
 }
 
 export default SpinningBox;
